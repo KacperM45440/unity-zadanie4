56e8f260478e5390b0cfa1f8f5febfbb69ffc387
KacperM45440 <104095161+KacperM45440@users.noreply.github.com>
2023-04-22 20:08:41 +0200

    Jednoczesne strzelanie z wielu miejsc

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -14,7 +14,12 @@ public class Shooter : MonoBehaviour
     [SerializeField] private float firingRateVariance = 0;
     [SerializeField] private float minimumFiringRate = 0.1f;
     [SerializeField] private bool useAI;
-    
+
+    [SerializeField] private enum Cannons { One = 1, Two = 2, Three = 3 }
+    [SerializeField] private Cannons cannonAmount;
+    private Vector3 cannonOffsetL;
+    private Vector3 cannonOffsetR;
+
     [HideInInspector]
     public bool isFiring;
 
@@ -56,21 +61,43 @@ public class Shooter : MonoBehaviour
     {
         while (true)
         {
-            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-
-            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-            if (rb != null)
+            List <GameObject> projectilesToSpawn = new();
+            switch ((int) cannonAmount)
             {
-                rb.velocity = moveDirection * projectileSpeed;
+                case 0:
+                    yield break;
+                case 1:
+                    projectilesToSpawn.Add(Instantiate(projectilePrefab, transform.position, Quaternion.identity));
+                    break;
+                case 2:
+                    cannonOffsetL = new Vector3(transform.position.x - 0.5f, transform.position.y + 0.75f);
+                    cannonOffsetR = new Vector3(transform.position.x + 0.5f, transform.position.y + 0.75f);
+                    projectilesToSpawn.Add(Instantiate(projectilePrefab, cannonOffsetL, Quaternion.identity));
+                    projectilesToSpawn.Add(Instantiate(projectilePrefab, cannonOffsetR, Quaternion.identity));
+                    break;
+                case 3:
+                    cannonOffsetL = new Vector3(transform.position.x - 0.5f, transform.position.y + 0.75f);
+                    cannonOffsetR = new Vector3(transform.position.x + 0.5f, transform.position.y + 0.75f);
+                    projectilesToSpawn.Add(Instantiate(projectilePrefab, cannonOffsetL, Quaternion.identity));
+                    projectilesToSpawn.Add(Instantiate(projectilePrefab, transform.position, Quaternion.identity));
+                    projectilesToSpawn.Add(Instantiate(projectilePrefab, cannonOffsetR, Quaternion.identity));
+                    break;
             }
-            
-            Destroy(projectile, projectileLifeTime);
 
-            float timeToNextProjectile =
-                Random.Range(baseFiringRate - firingRateVariance, baseFiringRate + firingRateVariance);
+            for (int i = 0; i<projectilesToSpawn.Count; i++)
+            {
+                Rigidbody2D rb = projectilesToSpawn[i].GetComponent<Rigidbody2D>();
+                
+                if (rb != null)
+                {
+                    rb.velocity = moveDirection * projectileSpeed;
+                }
 
+                Destroy(projectilesToSpawn[i], projectileLifeTime);
+            }
+
+            float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance, baseFiringRate + firingRateVariance);
             timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
-            
             yield return new WaitForSeconds(timeToNextProjectile);
         }
     }
